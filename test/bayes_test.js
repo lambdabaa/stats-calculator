def990a19d0bb7d35825bbce169f019bb7d3a7a7
lambdabaa <gareth@alumni.middlebury.edu>
2017-02-06 21:50:07 -0800

    Initial commit

---
+++ test/bayes_test.js
@@ -0,0 +1,30 @@
+const {assert} = require('chai');
+const bayes = require('../lib/bayes');
+const find = require('lodash/find');
+const reduce = require('lodash/reduce');
+
+suite('bayes', () => {
+  test('#computeBinaryProbabilityDistribution', () => {
+    const distribution = bayes.computeBinaryProbabilityDistribution([
+      true,
+      true,
+      true,
+      true,
+      false
+    ]);
+
+    const mostLikely = reduce(distribution, (result, p, x) => {
+      return p > result.p ? {x, p} : result;
+    }, {x: null, p: 0});
+
+    // If we see H, H, H, H, T then bias = 0.8 should be likely.
+    assert.equal(mostLikely.x, 0.8);
+  });
+
+  test('#predictBinaryBias', () => {
+    assert.equal(
+      bayes.predictBinaryBias([true, true, true, true, false]).prediction,
+      0.8
+    );
+  });
+});
